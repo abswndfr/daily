e2fb65a8f66ff3f6a1d1ccf82618c629f45f1a08
abswndfr <abswndfr@gmail.com>
2023-10-14 23:13:21 -0700

    ch8 10/14

--- CtCI/chap8.cpp
+++ CtCI/chap8.cpp
@@ -8,17 +8,31 @@ using namespace std;
 #define 	DP_SIZE		10
 int dp[DP_SIZE];
 
+/* 
+    0 - 1
+    1 - 1
+    2 - 2
+    3 - 4
+ */
+
 int stairCount(int steps)
 {
-    /*
-        0       1
-        1       1
-        2       2           1+1
-        3       1+2+1       1+2+1
-    */
-    memset(dp, -1, sizeof(int) * DP_SIZE);
+#if 1
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
 
-    if (steps < 2) {
+    if (dp[steps] == 0) {
+        dp[steps] = stairCount(steps - 1) + stairCount(steps - 2) + stairCount(steps - 3);
+    }
+
+    return dp[steps];
+#else
+    if (steps == 0) {
+        return 1;
+    }
+
+    if (steps == 1) {
         return 1;
     }
 
@@ -26,11 +40,8 @@ int stairCount(int steps)
         return 2;
     }
 
-    if (dp[steps] == -1) {
-        dp[steps] = stairCount(steps - 3) + stairCount(steps - 2) + stairCount(steps - 1);
-    }
-
-    return dp[steps];
+    return stairCount(steps - 1) + stairCount(steps - 2) + stairCount(steps - 3);
+#endif
 }
 
 
@@ -45,92 +56,79 @@ struct Point {
 //int** matrix = new int* [rows];
 //for (int i = 0; i < rows; ++i) 
 //  matrix[i] = new int[columns];
-int** createMatrix(int rows, int columns) {
+int** createMatrix(int rows, int columns) 
+{
+    int** matrix = new int* [rows];
 
-    int** matrix = new int* [rows];    
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = new int[columns];
+    }
 
-    for (int i = 0; i < rows; i++)
-        matrix[i] = new int[columns];        
-    //for (int* elem : matrix)
-    //    elem = new int[columns];
-
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < columns; j++)
-        matrix[i][j] = 1;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            matrix[i][j] = 1;
+        }
+    }
 
     return matrix;
 }
 
+
 #if 1 // w/ memoization
-bool getPath(int** matrix, int currRow, int currColumn, vector<Point*>& path, unordered_set<Point*>& duplicateVisits) 
+bool getPath(int** matrix, int row, int col, vector<Point*>& path, unordered_set<Point*>& duplicateVisits)
 {
-    if (currRow < 0 || currColumn < 0 || matrix[currRow][currColumn] == -1) {
+    if (row < 0 || col < 0 || matrix[row][col] == -1) {
         return false;
     }
 
-    Point* currPos = new Point(currRow, currColumn);
+    Point* p = new Point(row, col);
 
-    if (duplicateVisits.find(currPos) != duplicateVisits.end()) {
+    // 
+    if (duplicateVisits.find(p) != duplicateVisits.end()) {
         return false;
     }
 
-    bool atOrigin = currRow == 0 && currColumn == 0;
+    bool origin = (row == 0) && (col == 0);
 
-    if (atOrigin || getPath(matrix, currRow - 1, currColumn, path, duplicateVisits) || getPath(matrix, currRow, currColumn - 1, path, duplicateVisits)) {
-        path.push_back(currPos);
+    if (origin || getPath(matrix, row - 1, col, path, duplicateVisits) || getPath(matrix, row, col - 1, path, duplicateVisits)) {        
+        path.push_back(p);
         return true;
     }
 
-    duplicateVisits.insert(currPos);
+    //
+    duplicateVisits.insert(p);
 
     return false;
 }
 
 vector<Point*> getPath(int** matrix, int rows, int columns) {
-    vector<Point*> path;
+    vector<Point*> path;    
 
     unordered_set<Point*> duplicateVisits;
 
-    if (matrix == nullptr || rows == 0 || columns == 0) {
+    if ((matrix == nullptr) || rows == 0 || columns == 0) {
         return path;
     }
-    
-    if (getPath(matrix, rows - 1, columns - 1, path, duplicateVisits)) {
-        return path;
-    }
-    
+
+    getPath(matrix, rows - 1, columns - 1, path, duplicateVisits);
+
     return path;
 }
 #else // w/o memoization 
 bool getPath(int** matrix, int row, int col, vector<Point*>& path)
 {
-    printf("(%2d,%2d) ", row, col);
-    if (row < 0 || col < 0) {
-        printf(" B\n");
+    if (row < 0 || col < 0 || matrix[row][col] == -1) {
         return false;
     }
-
-    if (matrix[row][col] == -1) {
-        printf("-1\n");
-        return false;
-    }
-    printf("\n");
 
     bool origin = (row == 0) && (col == 0);
 
-    if (origin || getPath(matrix, row-1, col, path) || getPath(matrix, row, col - 1, path)) {
-        Point *p = new Point(row, col);
+    if (origin || getPath(matrix, row - 1, col, path) || getPath(matrix, row, col - 1, path)) {
+        Point* p = new Point(row, col);
         path.push_back(p);
-
-        for (Point* p : path) {
-            printf("(%2d,%2d)->", p->row, p->column);
-        }
-        cout << endl;
-
         return true;
     }
 
-    printf("(%2d,%2d)  R\n", row, col);
     return false;
 }
 
@@ -138,13 +136,11 @@ vector<Point*> getPath(int** matrix, int row, int col)
 {
     vector<Point*> path;
 
-    if (matrix == nullptr || row == 0 || col == 0) {
+    if ((matrix == nullptr) || row == 0 || col == 0) {
         return path;
     }
 
-    if (getPath(matrix, row - 1, col - 1, path)) {
-        return path;
-    }
+    getPath(matrix, row - 1, col - 1, path);
 
     return path;
 }
@@ -160,56 +156,43 @@ vector<int> arr = { 1, 2, 3 };
 
 void print_subsets(vector<vector<int>> result)
 {
-    for (vector<int> arr : result) {
-        for (int i : arr) {
-            cout << i << " ";
+    for (vector<int> subset : result) {
+        for (int i : subset) {
+            cout << i;
         }
-        cout << "." << endl;
+        cout << endl;
     }
 }
 
 void subsetsUtil(vector<vector<int>>& result, vector<int>& subset, int index)
 {
     result.push_back(subset);
-    //for (int a : subset) {
-    //    cout << a << " ";
-    //}
-    //cout << "*" << endl;
 
-    //for (int i = 0; i < arr.size(); i++) {
     for (int i = index; i < arr.size(); i++) {
         subset.push_back(arr[i]);
         subsetsUtil(result, subset, i + 1);
         subset.pop_back();
-
-        //result.push_back(subset);
     }
-}
+ }
 
 // CtCI - #2
-vector<vector<int>> get_subsets1(int* arr, int size)
+vector<vector<int>> get_subsets(int* arr, int idx, int size)
 {
     vector<vector<int>> result;
 
-    for (int i = 0; i < (1 << size); i++) {
-        vector<int> sub;
-#if 0
-        int n = i;
-        int index = 0;
-        while (n) {
-            if (n & 1) {
-                sub.push_back(arr[index]);
-            }
-            index++;
-            n = n >> 1;
-        }
-#else
-        for (int bit = 0; bit < size; bit++) {
-            if (i & (1 << bit)) {
-                sub.push_back(arr[bit]);
-            }
-        }
-#endif
+    if (idx == size) {
+        vector<int> empty;
+        result.push_back(empty);
+        return result;
+    }
+
+    vector<vector<int>> subs = get_subsets(arr, idx + 1, size);
+    int elem = arr[idx];
+
+    for (vector<int> sub : subs) {
+        result.push_back(sub);
+
+        sub.push_back(elem);
         result.push_back(sub);
     }
 
@@ -219,24 +202,21 @@ vector<vector<int>> get_subsets1(int* arr, int size)
 // CtCI - #1
 // it all starts w/ empty set
 // base cond'n : return not empty set but resutl with empty set
-vector<vector<int>> get_subsets(int* arr, int idx, int size)
+vector<vector<int>> get_subsets1(int* arr, int size)
 {
     vector<vector<int>> result;
 
-    if (idx == size) {
-        //vector<vector<int>> empty;
-        vector<int> empty;
-        result.push_back(empty);
-        return result;
-    }
+    for (int i=0; i < (1<<size); i++) {
+        vector<int> sub;
 
-    int a = arr[idx];
-    vector<vector<int>> subsets = get_subsets(arr, idx + 1, size);
+        int n = i;
+        for (int j = 0; j < size; j++) {
+            if (n&(1<<j)) {
+                sub.push_back(arr[j]);
+            }
+        }
 
-    for (vector<int> subset : subsets) {
-        result.push_back(subset);
-        subset.push_back(a);
-        result.push_back(subset);
+        result.push_back(sub);
     }
 
     return result;
@@ -244,11 +224,6 @@ vector<vector<int>> get_subsets(int* arr, int idx, int size)
 
 
 // 8.6
-#include <stdio.h>
-
-int N;
-int count;
-
 
 /*
 
@@ -258,12 +233,16 @@ int count;
 */
 
 // A: src, B: des, C: buf
-void Hanoi(int N, int A, int B, int C) {
-    if (N > 0) {
-        Hanoi(N - 1, A, C, B);
-        printf("moving %d from %d to %d\n", N, A, B);
-        Hanoi(N - 1, C, B, A);
+void Hanoi(int N, int A, int B, int C) 
+{
+    if (N == 0) {
+        return;
     }
+
+    Hanoi(N-1, A, C, B);
+    printf("disk %d from %d to %d\n", N, A, B);
+    Hanoi(N-1, C, B, A);
+
 }
 
 // 8.7
@@ -273,19 +252,21 @@ typedef vector<string> vs;
 vs permu(string s)
 {
     vs result;
-    if (s == "") {
-        result.push_back(s);
+
+    if (s.length() == 0) {
+        result.push_back("");
         return result;
     }
 
     for (int i = 0; i < s.length(); i++) {
-        string c = s.substr(i,1);
         string t = s;
+        string c = t.substr(i,1);
 
-        vs subs = permu(t.erase(i, 1));
+        vs subs;
+        subs = permu(t.erase(i,1));
 
-        for (string sub : subs) {        
-            result.push_back(sub + c);
+        for (string sub : subs) {
+            result.push_back(c+sub);
         }
     }
 
@@ -299,11 +280,6 @@ int main()
     for (int i = 0; i < DP_SIZE; i++) {
         dp[i] = 0;
     }
-
-    dp[0] = 1;
-    dp[1] = 1;
-    dp[2] = 2;
-    dp[3] = 4;
 
     //printf("6 step -> %d ways\n", countWays(6));
 
@@ -401,14 +377,14 @@ int main()
 
 
     // 8.6
-    N = 3;
+    int N = 3;
     Hanoi(N, 1, 2, 3);
 
 
     // 8.7
     string s = "abc";
-    vs res = permu(s);                  // sol'n #1 : add a_n at every possible spot in P(a_n-1)
-    //vs res = permu1(s);                   // sol'n #2 : i + P(before + after) 
+    //vs res = permu1(s);                  // sol'n #1 : add a_n at every possible spot in P(a_n-1)
+    vs res = permu(s);                   // sol'n #2 : i + P(before + after) 
     for (int i = 0; i < res.size(); ++i)
         cout << res[i] << endl;
 
